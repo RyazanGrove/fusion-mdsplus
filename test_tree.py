092db077f34b711e1b26312e0c454c0c03e2fd06
Raskhodchikov Mikhail <mikhail.raskhodchikov@aalto.fi>
2021-05-12 16:47:12 +0300

    testing for grid has been implemented

--- test_tree.py
+++ test_tree.py
@@ -377,8 +377,226 @@ def compare_trees(first_tree, second_tree):
         print("SNAPSHOT.GRID.CZ_Y.COMMENT are different")
         difference_count += 1
 
+    #SNAPSHOT.GRID.DSPAR.data
+    if not (tree1.SNAPSHOT.GRID.DSPAR.data() == tree2.SNAPSHOT.GRID.DSPAR.data()).all():
+        print("SNAPSHOT.GRID.DSPAR are different")
+        difference_count += 1
 
+    #SNAPSHOT.GRID.DSPAR.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.DSPAR.BASISNAME.data() != tree2.SNAPSHOT.GRID.DSPAR.BASISNAME.data():
+        print("SNAPSHOT.GRID.DSPAR.BASISNAME are different")
 
+    #SNAPSHOT.GRID.DSPAR.COMMENT.data
+    if tree1.SNAPSHOT.GRID.DSPAR.COMMENT.data() != tree2.SNAPSHOT.GRID.DSPAR.COMMENT.data():
+        print("SNAPSHOT.GRID.DSPAR.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSPOL.data
+    if not (tree1.SNAPSHOT.GRID.DSPOL.data() == tree2.SNAPSHOT.GRID.DSPOL.data()).all():
+        print("SNAPSHOT.GRID.DSPOL are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSPOL.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.DSPOL.BASISNAME.data() != tree2.SNAPSHOT.GRID.DSPOL.BASISNAME.data():
+        print("SNAPSHOT.GRID.DSPOL.BASISNAME are different")
+
+    #SNAPSHOT.GRID.DSPOL.COMMENT.data
+    if tree1.SNAPSHOT.GRID.DSPOL.COMMENT.data() != tree2.SNAPSHOT.GRID.DSPOL.COMMENT.data():
+        print("SNAPSHOT.GRID.DSPOL.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSRAD.data
+    if not (tree1.SNAPSHOT.GRID.DSRAD.data() == tree2.SNAPSHOT.GRID.DSRAD.data()).all():
+        print("SNAPSHOT.GRID.DSRAD are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSRAD.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.DSRAD.BASISNAME.data() != tree2.SNAPSHOT.GRID.DSRAD.BASISNAME.data():
+        print("SNAPSHOT.GRID.DSRAD.BASISNAME are different")
+
+    #SNAPSHOT.GRID.DSRAD.COMMENT.data
+    if tree1.SNAPSHOT.GRID.DSRAD.COMMENT.data() != tree2.SNAPSHOT.GRID.DSRAD.COMMENT.data():
+        print("SNAPSHOT.GRID.DSRAD.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSTARG1.data
+    if not (tree1.SNAPSHOT.GRID.DSTARG1.data() == tree2.SNAPSHOT.GRID.DSTARG1.data()).all():
+        print("SNAPSHOT.GRID.DSTARG1 are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSTARG1.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.DSTARG1.BASISNAME.data() != tree2.SNAPSHOT.GRID.DSTARG1.BASISNAME.data():
+        print("SNAPSHOT.GRID.DSTARG1.BASISNAME are different")
+
+    #SNAPSHOT.GRID.DSTARG1.COMMENT.data
+    if tree1.SNAPSHOT.GRID.DSTARG1.COMMENT.data() != tree2.SNAPSHOT.GRID.DSTARG1.COMMENT.data():
+        print("SNAPSHOT.GRID.DSTARG1.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSTARG2.data
+    if not (tree1.SNAPSHOT.GRID.DSTARG2.data() == tree2.SNAPSHOT.GRID.DSTARG2.data()).all():
+        print("SNAPSHOT.GRID.DSTARG2 are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.DSTARG2.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.DSTARG2.BASISNAME.data() != tree2.SNAPSHOT.GRID.DSTARG2.BASISNAME.data():
+        print("SNAPSHOT.GRID.DSTARG2.BASISNAME are different")
+
+    #SNAPSHOT.GRID.DSTARG2.COMMENT.data
+    if tree1.SNAPSHOT.GRID.DSTARG2.COMMENT.data() != tree2.SNAPSHOT.GRID.DSTARG2.COMMENT.data():
+        print("SNAPSHOT.GRID.DSTARG2.COMMENT are different")
+        difference_count += 1
+
+    try:
+        #SNAPSHOT.GRID.DSTARG3.data
+        if not (tree1.SNAPSHOT.GRID.DSTARG3.data() != tree2.SNAPSHOT.GRID.DSTARG3.data().all()):
+            print("SNAPSHOT.GRID.DSTARG3 are different")
+            difference_count += 1
+
+        #SNAPSHOT.GRID.DSTARG3.BASISNAME.data
+        if tree1.SNAPSHOT.GRID.DSTARG3.BASISNAME.data() != tree2.SNAPSHOT.GRID.DSTARG3.BASISNAME.data():
+            print("SNAPSHOT.GRID.DSTARG3.BASISNAME are different")
+            difference_count += 1
+
+        #SNAPSHOT.GRID.DSTARG3.COMMENT.data
+        if tree1.SNAPSHOT.GRID.DSTARG3.COMMENT.data() != tree2.SNAPSHOT.GRID.DSTARG3.COMMENT.data():
+            print("SNAPSHOT.GRID.DSTARG3.COMMENT are different")
+            difference_count += 1
+    except MDSplus.mdsExceptions.TreeNODATA as ex:
+        print("No data in SNAPSHOT.GRID.DSTARG3.data")
+
+    try:
+        #SNAPSHOT.GRID.DSTARG4.data
+        if not (tree1.SNAPSHOT.GRID.DSTARG4.data() != tree2.SNAPSHOT.GRID.DSTARG4.data().all()):
+            print("SNAPSHOT.GRID.DSTARG4 are different")
+            difference_count += 1
+
+        #SNAPSHOT.GRID.DSTARG4.BASISNAME.data
+        if tree1.SNAPSHOT.GRID.DSTARG4.BASISNAME.data() != tree2.SNAPSHOT.GRID.DSTARG4.BASISNAME.data():
+            print("SNAPSHOT.GRID.DSTARG4.BASISNAME are different")
+            difference_count += 1
+
+        #SNAPSHOT.GRID.DSTARG4.COMMENT.data
+        if tree1.SNAPSHOT.GRID.DSTARG4.COMMENT.data() != tree2.SNAPSHOT.GRID.DSTARG4.COMMENT.data():
+            print("SNAPSHOT.GRID.DSTARG4.COMMENT are different")
+            difference_count += 1
+    except MDSplus.mdsExceptions.TreeNODATA as ex:
+        print("No data in SNAPSHOT.GRID.DSTARG4.data")
+
+    #SNAPSHOT.GRID.PSINRAD.data
+    if not (tree1.SNAPSHOT.GRID.PSINRAD.data() == tree2.SNAPSHOT.GRID.PSINRAD.data()).all():
+        print("SNAPSHOT.GRID.PSINRAD are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.PSINRAD.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.PSINRAD.BASISNAME.data() != tree2.SNAPSHOT.GRID.PSINRAD.BASISNAME.data():
+        print("SNAPSHOT.GRID.PSINRAD.BASISNAME are different")
+
+    #SNAPSHOT.GRID.PSINRAD.COMMENT.data
+    if tree1.SNAPSHOT.GRID.PSINRAD.COMMENT.data() != tree2.SNAPSHOT.GRID.PSINRAD.COMMENT.data():
+        print("SNAPSHOT.GRID.PSINRAD.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.REGVOL.data
+    if not (tree1.SNAPSHOT.GRID.REGVOL.data() == tree2.SNAPSHOT.GRID.REGVOL.data()).all():
+        print("SNAPSHOT.GRID.REGVOL are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.REGVOL.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.REGVOL.BASISNAME.data() != tree2.SNAPSHOT.GRID.REGVOL.BASISNAME.data():
+        print("SNAPSHOT.GRID.REGVOL.BASISNAME are different")
+
+    #SNAPSHOT.GRID.REGVOL.COMMENT.data
+    if tree1.SNAPSHOT.GRID.REGVOL.COMMENT.data() != tree2.SNAPSHOT.GRID.REGVOL.COMMENT.data():
+        print("SNAPSHOT.GRID.REGVOL.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.CZ.data
+    if not (tree1.SNAPSHOT.GRID.CZ.data() == tree2.SNAPSHOT.GRID.CZ.data()).all():
+        print("SNAPSHOT.GRID.CZ are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.CZ.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.CZ.BASISNAME.data() != tree2.SNAPSHOT.GRID.CZ.BASISNAME.data():
+        print("SNAPSHOT.GRID.CZ.BASISNAME are different")
+
+    #SNAPSHOT.GRID.CZ.COMMENT.data
+    if tree1.SNAPSHOT.GRID.CZ.COMMENT.data() != tree2.SNAPSHOT.GRID.CZ.COMMENT.data():
+        print("SNAPSHOT.GRID.CZ.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.R.data
+    if not (tree1.SNAPSHOT.GRID.R.data() == tree2.SNAPSHOT.GRID.R.data()).all():
+        print("SNAPSHOT.GRID.R are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.R.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.R.BASISNAME.data() != tree2.SNAPSHOT.GRID.R.BASISNAME.data():
+        print("SNAPSHOT.GRID.R.BASISNAME are different")
+
+    #SNAPSHOT.GRID.R.COMMENT.data
+    if tree1.SNAPSHOT.GRID.R.COMMENT.data() != tree2.SNAPSHOT.GRID.R.COMMENT.data():
+        print("SNAPSHOT.GRID.R.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.Z.data
+    if not (tree1.SNAPSHOT.GRID.Z.data() == tree2.SNAPSHOT.GRID.Z.data()).all():
+        print("SNAPSHOT.GRID.Z are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.Z.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.Z.BASISNAME.data() != tree2.SNAPSHOT.GRID.Z.BASISNAME.data():
+        print("SNAPSHOT.GRID.Z.BASISNAME are different")
+
+    #SNAPSHOT.GRID.Z.COMMENT.data
+    if tree1.SNAPSHOT.GRID.Z.COMMENT.data() != tree2.SNAPSHOT.GRID.Z.COMMENT.data():
+        print("SNAPSHOT.GRID.Z.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.VESSEL.data
+    if not (tree1.SNAPSHOT.GRID.VESSEL.data() == tree2.SNAPSHOT.GRID.VESSEL.data()).all():
+        print("SNAPSHOT.GRID.VESSEL are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.VESSEL.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.VESSEL.BASISNAME.data() != tree2.SNAPSHOT.GRID.VESSEL.BASISNAME.data():
+        print("SNAPSHOT.GRID.VESSEL.BASISNAME are different")
+
+    #SNAPSHOT.GRID.VESSEL.COMMENT.data
+    if tree1.SNAPSHOT.GRID.VESSEL.COMMENT.data() != tree2.SNAPSHOT.GRID.VESSEL.COMMENT.data():
+        print("SNAPSHOT.GRID.VESSEL.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.ZA.data
+    if not (tree1.SNAPSHOT.GRID.ZA.data() == tree2.SNAPSHOT.GRID.ZA.data()).all():
+        print("SNAPSHOT.GRID.ZA are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.ZA.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.ZA.BASISNAME.data() != tree2.SNAPSHOT.GRID.ZA.BASISNAME.data():
+        print("SNAPSHOT.GRID.ZA.BASISNAME are different")
+
+    #SNAPSHOT.GRID.ZA.COMMENT.data
+    if tree1.SNAPSHOT.GRID.ZA.COMMENT.data() != tree2.SNAPSHOT.GRID.ZA.COMMENT.data():
+        print("SNAPSHOT.GRID.ZA.COMMENT are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.ZN.data
+    if not (tree1.SNAPSHOT.GRID.ZN.data() == tree2.SNAPSHOT.GRID.ZN.data()).all():
+        print("SNAPSHOT.GRID.ZN are different")
+        difference_count += 1
+
+    #SNAPSHOT.GRID.ZN.BASISNAME.data
+    if tree1.SNAPSHOT.GRID.ZN.BASISNAME.data() != tree2.SNAPSHOT.GRID.ZN.BASISNAME.data():
+        print("SNAPSHOT.GRID.ZN.BASISNAME are different")
+
+    #SNAPSHOT.GRID.ZN.COMMENT.data
+    if tree1.SNAPSHOT.GRID.ZN.COMMENT.data() != tree2.SNAPSHOT.GRID.ZN.COMMENT.data():
+        print("SNAPSHOT.GRID.ZN.COMMENT are different")
+        difference_count += 1
+
+    # end of SNAPSHOT.GRID
+    # -------------------------------------------------------------------------
 
     if difference_count == 0:
         print("The trees are same")
